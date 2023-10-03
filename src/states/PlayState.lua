99bc58484221120f97cc801f21f7d687a4169110
liamalexfarmer <liamalexfarmer@gmail.com>
2023-10-03 14:16:27 -0400

    added brick collision and paddle momentum influence--has some bugs with multiple brick collision instances

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -16,11 +16,14 @@ function PlayState:init()
 
 	--starting velocity for the ball
 	self.ball.dx = math.random(-200, 200)
-	self.ball.dy = math.random(-50, 60)
+	self.ball.dy = math.random(-60, -40)
+	--this is to impliment conservation of momentum later on, so when dx or dy are manipulated, their collective speed remains constant
+	--unless of course we want it to conitnue to accelerate
+	self.ball.m = math.abs(self.ball.dx) + math.abs(self.ball.dy)
 
 	--init the ball in the center
 	self.ball.x = VIRTUAL_WIDTH / 2 - 4
-	self.ball.y = VIRTUAL_HEIGHT - 42
+	self.ball.y = VIRTUAL_HEIGHT - 60
 
 	--establishing the pause state as false until otherwise instantiated
 	self.paused = false
@@ -54,7 +57,30 @@ function PlayState:update(dt)
 		--reverse y direction
 		self.ball.dy = -self.ball.dy
 
-		
+		--implimentation of code that changes the x velocity of the ball depending on where on the paddle it's hit
+		--allows strategy of intent on behalf of the player
+
+		--if the ball is on the left side of the paddle and the paddle is moving left
+		if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.dx < 0 then
+			self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
+
+--[[conserving momentum by recalculating dy; remove square brackets to enable
+			self.ball.dy = 
+			--returns 1 or -1 depending on direction of dy. purely to provide polarity
+			(self.ball.dy/math.abs(self.ball.dy))
+			--ball.dm is designed not to care about directionality, so we're basically subtracting the new dx from the previous collective speed
+			--multiplied by the defined polarity by leveraging absolute value of ball.dy
+			 * (self.ball.m - math.abs(self.ball.dx))
+			--recalculate ball.m for future usage
+			self.ball.m = (math.abs(self.ball.dx) + math.abs(self.ball.dy)) * 1.03
+]]
+
+		elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.dx > 0 then
+			self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+			--self.ball.dy = (self.ball.dy / math.abs(self.ball.dy)) * (self.ball.m - math.abs(self.ball.dx))
+			--self.ball.m = (math.abs(self.ball.dx) + math.abs(self.ball.dy)) * 1.03
+		end
+
 		gSounds.paddleHit:play()
 	end
 
@@ -66,6 +92,35 @@ function PlayState:update(dt)
 
 			--trigger a collision
 			brick:hit()
+
+			--code that handles brick collisions
+			--uses velocity as a check for whether collision could/may occur on a specific brick edge
+			--defining the brick edge where collision occurs is necessary for determining the reflection pattern of the ball
+
+			if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+				self.ball.dx = -self.ball.dx
+				self.ball.x = brick.x - self.ball.width
+
+			elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
+				self.ball.dx = self.ball.dx
+				self.ball.x = brick.x + brick.width
+
+			elseif self.ball.y < brick.y then
+
+				self.ball.dy = -self.ball.dy
+				self.ball.y = brick.y - 8
+			else
+
+				self.ball.dy = -self.ball.dy
+				self.ball.y = brick.y + 16
+			end
+
+			--make the ball go slightly faster
+			self.ball.dy = self.ball.dy * 1.01
+			self.ball.dx = self.ball.dx * 1.01
+
+			--collide with one brick only (helps with corners)
+			break
 		end
 	end
 
